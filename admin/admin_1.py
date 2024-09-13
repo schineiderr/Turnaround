9b5284492db8b4d0bf1972fd2cc726b89ff9ace6
Isabela <isabeb11@braskem.com>
2024-09-13 17:46:31 -0300

    update admin page

--- admin/admin_1.py
+++ admin/admin_1.py
@@ -1,10 +1,26 @@
 import streamlit as st
-from utils import lista_usuarios
+from utils import lista_usuarios, conn
 
 st.header("Admin 1")
 st.write(f"Você está logado como {st.session_state['role']}.")
 
 placeholder = st.empty()
 with placeholder.container(border=True):
-   st.markdown("### Listagem de Usuários")
-   st.table(lista_usuarios)
+   st.markdown("### Usuários")
+   edited_lista_usuarios = st.data_editor(lista_usuarios,
+                                          use_container_width=True,
+                                          hide_index=True,
+                                          num_rows="dynamic",
+                                          column_config={
+                                             "permission": st.column_config.SelectboxColumn(options=["Solicitante", "Membro", "Admin"])
+                                          })
+
+   col1,col2,col3 = st.columns([2,1,2])
+   with col2:
+      save_button = st.button("Salvar")
+   if save_button:
+      try:
+         df = conn.update(worksheet="usuarios",data=edited_lista_usuarios)
+         st.success("Usuários atualizados com sucesso!")
+      except:
+         st.error("Não foi possível atualizar! Contate o desenvolvedor")
